3de0457f39957615c0d37b97b1afe3ec18466ff8
Jocelyn Rodriguez <jocelynrodriguez@Jocelyns-MacBook-Air.local>
2017-11-11 00:53:06 -0800

    parsed data

--- map.js
+++ map.js
@@ -9,19 +9,22 @@ url += '?' + $.param({
 function parseResult(results){
 	console.log('parsing result....');
 	var articles = results['results'];
-	// console.log(articles);
+	console.log(articles);
 
 	var data = {};
 	var arrdata = [];
 
 	for(var i = 0; i < articles.length; i++){
-		// console.log(articles[i]['title']);
-		data = {
-			title: articles[i]['title'],
-			location: articles[i]['geo_facet'][0],
-			url: articles[i]['url']
+		//console.log(articles[i]['title']);
+		if(articles[i]['geo_facet'][0] !== undefined){
+			data = {
+				title: articles[i]['title'],
+				location: articles[i]['geo_facet'][0],
+				url: articles[i]['url']
+			}
+			arrdata.push(data);
 		}
-		arrdata.push(data);
+
 	}
 		console.log(arrdata);
 		return arrdata;
