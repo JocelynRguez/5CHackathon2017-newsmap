835d6530e19b52b1a93bb61e53974d2d5f7f6f16
Jocelyn Rodriguez <jocelynrodriguez@Jocelyns-MacBook-Air.local>
2017-11-11 00:26:07 -0800

    parse data update

--- map.js
+++ map.js
@@ -5,11 +5,35 @@ url += '?' + $.param({
   'api-key': "20ec7403e3f64b35b7abeef0bb5dfb4b"
 });
 
+
+function parseResult(results){
+	console.log('parsing result....');
+	var articles = results['results'];
+	// console.log(articles);
+
+	var data = {};
+	var arrdata = [];
+
+	for(var i = 0; i < articles.length; i++){
+		// console.log(articles[i]['title']);
+		data = {
+			title: articles[i]['title'],
+			location: articles[i]['geo_facet'][0],
+			url: articles[i]['url']
+		}
+		arrdata.push(data);
+	}
+		console.log(arrdata);
+		return arrdata;
+}
+
+
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-  console.log(result);
+  //console.log(result);
+	parseResult(result);
 }).fail(function(err) {
   throw err;
 });
